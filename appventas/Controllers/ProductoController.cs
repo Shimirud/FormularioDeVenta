f01ff50cbbdf026b0c3308d0b14d4604fd945632
michellecuevamendez@hotmail.com <michellecuevamendez@hotmail.com>
2021-03-25 22:28:31 -0500

    Actualización

---
+++ appventas/Controllers/ProductoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using appventas.Models;
+
+namespace appventas.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ILogger<ProductoController> _logger;
+
+        public ProductoController(ILogger<ProductoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MiMetodoExecute(Producto objProducto)
+        {
+            Double result = 0.0;
+            String message ="";
+            
+                result = (objProducto.Precio * objProducto.Cantidad)*1.18;
+                message ="El resultado es " + result;
+            
+            
+            ViewData["Message"] = message;
+            return View("Index");
+        }
+
+    }
+}
